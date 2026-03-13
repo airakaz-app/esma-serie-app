10b5e204d1b72569aa1b47966d82bdade79787ab
airakaz-app <61122844+airakaz-app@users.noreply.github.com>
2026-03-13 16:28:49 -0400

    Fix add-series modal to refresh list as soon as series is created

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/series-infos');
 Route::get('/series-infos', [SeriesInfoController::class, 'index'])->name('series-infos.index');
+Route::post('/series-infos/scrape', [SeriesInfoController::class, 'scrape'])->name('series-infos.scrape');
+Route::get('/series-infos/scrape-status/{trackingKey}', [SeriesInfoController::class, 'scrapeStatus'])->name('series-infos.scrape-status');
 Route::get('/series-infos/{seriesInfo}', [SeriesInfoController::class, 'show'])->name('series-infos.show');
