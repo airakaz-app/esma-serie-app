99842dc20a7c81db85e70d0e059af0522f932526
airakaz-app <ouadad.airakaz@gmail.com>
2026-03-28 00:51:27 -0400

    fix bug

--- app/Services/Scraper/ScraperSecurityService.php
+++ app/Services/Scraper/ScraperSecurityService.php
@@ -95,7 +95,7 @@ class ScraperSecurityService
             'User-Agent'      => self::randomUserAgent(),
             'Accept'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Language' => 'fr-FR,fr;q=0.9,en;q=0.8',
-            'Accept-Encoding' => 'gzip, deflate, br',
+            'Accept-Encoding' => 'gzip, deflate',
             'DNT'             => '1',
             'Connection'      => 'keep-alive',
             'Upgrade-Insecure-Requests' => '1',
